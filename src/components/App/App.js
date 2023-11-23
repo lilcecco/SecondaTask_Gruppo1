ddb1c33cffa0f0ee8cd9a9b84ea7d91d7a4ea1e6
Matteo <maoce007@gmail.com>
2023-11-23 17:26:12 +0100

    [Header] aggiunto menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
+import Home from '../Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-primary min-h-screen px-20">
-      <div className="flex items-center">
-        <span className="text-secondary text-3xl">#</span>
-        <span className="text-white text-3xl">progetti</span>
-        <span className="border-b border-white-500 flex-grow ml-4 w-1" ></span>
-      </div>
+    <div>
+      <Home />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,40 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-brown min-h-screen">
+      <header className="flex justify-between px-20 py-10">
+        <div className="logo"></div>
+        <nav>
+          <ul className="flex gap-x-10">
+            <li>
+              <span className="text-green text-lg">#</span>
+              <span className="text-white text-lg">home</span>
+            </li>
+            <li>
+              <span className="text-green text-lg">#</span>
+              <span className="text-gray text-lg">skills</span>
+            </li>
+            <li>
+              <span className="text-green text-lg">#</span>
+              <span className="text-gray text-lg">progetti</span>
+            </li>
+            <li>
+              <span className="text-green text-lg">#</span>
+              <span className="text-gray text-lg">about</span>
+            </li>
+            <li>
+              <span className="text-green text-lg">#</span>
+              <span className="text-gray text-lg">contatti</span>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="flex items-center px-20">
+        <span className="text-green text-3xl">#</span>
+        <span className="text-white text-3xl">progetti</span>
+        <span className="border-b border-white-500 flex-grow ml-4 w-1" ></span>
+      </div>
+    </div>
   )
 }
 
